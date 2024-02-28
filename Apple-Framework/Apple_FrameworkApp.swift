89addce7e6a9cf6da70e3a154a1a51cb36f7e3e1
Ritik Raman <ritikraman01@gmail.com>
2024-02-28 21:31:37 +0530

    Initial Commit

---
+++ Apple-Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple-Framework
+//
+//  Created by Ritik Raman on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
