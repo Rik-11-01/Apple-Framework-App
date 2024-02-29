83377375bea6308a32a7991c152464332239ea92
Ritik Raman <ritikraman01@gmail.com>
2024-02-29 18:37:21 +0530

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- Apple-Framework/FrameworkGridView.swift
+++ Apple-Framework/FrameworkGridView.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem]=[GridItem(.flexible()),
+                             GridItem(.flexible()),
+                             GridItem(.flexible())
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LazyVGrid(columns:columns){
+            Frameworktitleview(imagename: "arkit", name: "Arkit")
+            Frameworktitleview(imagename: "arkit", name: "Arkit")
+            Frameworktitleview(imagename: "arkit", name: "Arkit")
+            Frameworktitleview(imagename: "arkit", name: "Arkit")
+            Frameworktitleview(imagename: "arkit", name: "Arkit")
+            Frameworktitleview(imagename: "arkit", name: "Arkit")
+        }
+
     }
 }
 
 #Preview {
+    
     FrameworkGridView()
+}
+
+struct Frameworktitleview: View{
+    let imagename:String
+    let name:String
+    
+    var body :some View{
+        VStack{
+            Image(imagename)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+    }
+    
 }
