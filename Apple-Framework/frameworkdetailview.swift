ea03b31b191e8c23c90ceae04df92fab3ab26be5
Ritik Raman <ritikraman01@gmail.com>
2024-02-29 18:37:21 +0530

    Merge remote-tracking branch 'refs/remotes/origin/main'

---
+++ Apple-Framework/frameworkdetailview.swift
@@ -0,0 +1,50 @@
+//
+//  frameworkdetailview.swift
+//  Apple-Framework
+//
+//  Created by Ritik Raman on 02/03/24.
+//
+
+import SwiftUI
+
+struct frameworkdetailview: View {
+    var framework:Framework
+    var body: some View {
+        VStack{
+            HStack{
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+                .padding()
+            }
+
+            Spacer()
+            Frameworktitleview(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            Spacer()
+            Button{
+                
+            }label: {
+                Text("Learn More")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .frame(width:280,height:50)
+                    .background(Color.red)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(10)
+            }
+        }
+    }
+}
+
+#Preview {
+    frameworkdetailview(framework: MockData.sampleFramework)
+}
